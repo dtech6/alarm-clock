50472695f99cc79bd5e05a49ec7a952ab0e48a90
dtech06 <darshil.tech06@gmail.com>
2023-05-23 01:05:30 +0530

    Project Completed.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import AlarmContainer from "./Components/AlarmContainer/AlarmContainer";
+import Clock from "./Components/Clock/Clock";
+import { useEffect } from "react";
+import { setAlarmsFromStorage } from "./Redux/actions/AlarmAction";
 
 function App() {
+  const activeAlarms = useSelector((state) => state.alarmReducer.activeAlarm);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const myAlarms = localStorage.getItem("myAlarms");
+    if (myAlarms) {
+      dispatch(setAlarmsFromStorage(JSON.parse(myAlarms)));
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Alarm Clock</h1>
+    <div
+      className="App"
+      style={{ backgroundColor: activeAlarms ? activeAlarms.bgColor : "#000" }}
+    >
+      <Clock />
+      <AlarmContainer />
     </div>
   );
 }
